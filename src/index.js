4f3eeae212b04acdcadb5e0e931fd69baafa82fa
Brammont <brayanmazuera1@gmail.com>
2021-11-13 18:51:02 -0500

    conectado la base de datos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-const app = require('./server')
+require('dotenv').config();
 
+const app = require('./server')
+require('./database')
+console.log()
 
 app.listen(app.get('port'),()=>{
     console.log('server on port',app.get('port'))
