0d6656224fecb214cf7b29f47c61388cf1c33571
Brammont <brayanmazuera1@gmail.com>
2021-11-13 21:00:39 -0500

    conextando express-handlebars y las vistas de la app

--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const path = require('path')
 const app = express();
+const { create } = require('express-handlebars');
 
 //inicializaciones
 
 //configuraciones
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname ,'views'));
+const hbs = create({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+})
+app.engine('handlebars', hbs.engine);
+  app.set("view engine", ".hbs");
+  
+
 //midlewares
 app.use(express.urlencoded({extended:false}));
 
