e69742c0bc9955e05417f1ad8fb3886a023f4243
Brammont <brayanmazuera1@gmail.com>
2021-11-14 00:07:59 -0500

    configurando controaldores para las rutas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/',(req,res)=>{
-    res.render('index')
-})
-router.get('/about',(req,res)=>{
-    res.render('about')
-})
+const { renderIndex,renderAbout } = require('../controllers/index.controllers')
+router.get('/',renderIndex)
+router.get('/about',renderAbout)
 module.exports= router;
