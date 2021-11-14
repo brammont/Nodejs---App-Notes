71e2a0c091020cfaed6ade44d4a1abed8d10d04c
Brammont <brayanmazuera1@gmail.com>
2021-11-13 19:18:34 -0500

    conenctado la base de datos con variables de entorno

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const app = require('./server')
 require('./database')
-console.log()
+//console.log(process.env)
 
 app.listen(app.get('port'),()=>{
     console.log('server on port',app.get('port'))
